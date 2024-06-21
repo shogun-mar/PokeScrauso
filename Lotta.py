403cf85c57a8c8ab5bf4408473c5d1b6a075127e
Shakkov68 <mariocaccamo2016@gmail.com>
2024-06-21 17:42:13 +0200

    commit

--- Lotta.py
+++ Lotta.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import time
 
 # Inizializza Pygame
 pygame.init()
@@ -10,6 +11,7 @@ SCREEN_HEIGHT = 480
 SCREEN_TITLE = "Pokémon Battle Menu" 
 HEALTH_BAR_WIDTH=98
 HEALTH_BAR_HEIGHT=7
+TRAINER_IMAGE_DELAY = 0.1  # Delay between trainer images in seconds
 
 # Crea lo schermo
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -18,10 +20,10 @@ font=pygame.font.Font("graphics/fonts/PressStart2P.ttf",12)
 
 
 # Carica le immagini dei Pokémon
-name_gioc="DITTO"
+name_gioc="PIKACHU"
 life_point=100
 poke_life=100
-player_pokemon_img = pygame.image.load('graphics/Gen 4 Pokemon Back/132 HGSS-B.png')
+player_pokemon_img = pygame.image.load('graphics/Pokemon/Back/PIKACHU.png')
 player_pokemon_rect = player_pokemon_img.get_rect(center=(130, 400))
 player_riq_img=pygame.image.load('graphics/UI/Battle/databox_normal.png')
 player_riq_rect=player_riq_img.get_rect(center=(129,260))
@@ -31,8 +33,8 @@ player_info_text = font.render(f"{name_gioc} Lvl {1000} ", True, (0, 0, 0))
 name_enemy="PIKACHU"
 enemy_hp=40
 enemy_tot_health=40
-enemy_pokemon_img = pygame.image.load('graphics/HGSS/25 HGSS.png ')
-enemy_pokemon_rect = enemy_pokemon_img.get_rect(center=(580, 220))
+enemy_pokemon_img = pygame.image.load('graphics/Pokemon/Front/PIKACHU.png ')
+enemy_pokemon_rect = enemy_pokemon_img.get_rect(center=(580, 250))
 enemy_riq_img=pygame.image.load('graphics/UI/Battle/databox_normal_foe.png')
 enemy_riq_rect=enemy_riq_img.get_rect(center=(599,60))
 enemy_info_text = font.render(f"{name_enemy} Lvl {104} ", True, (0, 0, 0))
@@ -45,15 +47,27 @@ for i in range(18):
     pokeball_images.append(pokeball_image)
 
 pygame.mixer.init()
-pokemon_cry = pygame.mixer.Sound('sounds/cries/132.ogg')
+pokemon_cry = pygame.mixer.Sound('sounds/cries/26.ogg')
 pokeball_x = 200
 pokeball_y = 300
 pokeball_animation_speed = 3.65
 pokeball_frame = 0
-pokeball_index = 0
+pokeball_index = 2
 pokemon_animation_speed = 2
 pokemon_frame = 0
 pokemon_index = 0
+
+trainer_images=[]
+trainer_img=pygame.image.load('graphics/battle/FRLG Trainer Backs 01.png')
+trainer_images.append(trainer_img)
+trainer_img=pygame.image.load('graphics/battle/FRLG Trainer Backs 04.png')
+trainer_images.append(trainer_img)
+trainer_img=pygame.image.load('graphics/battle/FRLG Trainer Backs 05.png')
+trainer_images.append(trainer_img)
+trainer_index=0
+trainer_image_counter = 0
+trainer_image_timer = time.time()
+
 
 # Definisci i bottoni
 attack_button = pygame.Rect(400, 360, 150, 50)
@@ -107,7 +121,8 @@ poke4_mode=False
 poke5_mode=False
 poke6_mode=False
 running=True
-
+pokemon_drawn = False
+pokeball_drawn=False
 
 
 
@@ -127,7 +142,7 @@ while running:
             elif move2_button.collidepoint(event.pos) and battle_mode:
                 print("Mossa 2 selezionata!")
             elif move3_button.collidepoint(event.pos) and battle_mode:
-                print("Mossa 3 selezionata!")
+                print("Mossa 3selezionata!")
             elif move4_button.collidepoint(event.pos) and battle_mode:
                 print("Mossa 4 selezionata!")
             elif poke_button.collidepoint(event.pos) and not pokemon_mode and not battle_mode and not bag_mode and not run_mode:
@@ -180,92 +195,146 @@ while running:
     player_life=font.render(f"{life_point}/{poke_life}", True,(0,0,0))
     # Disegna lo sfondo
     screen.blit(battle_background, (0, 0))
-    """
-    screen.blit(pokeball_images[pokeball_frame], (pokeball_x, pokeball_y))
+    if not pokemon_drawn:
+        # Update trainer animation
+        if not pokeball_drawn:
+            if trainer_index >= len(trainer_images):
+                trainer_index = 0
 
-    # Aggiorna la posizione della Poké Ball
-    pokeball_x += pokeball_speed
-    if pokeball_x > 250:
-        pokeball_speed = -pokeball_speed
-    if pokeball_x < 200:
-        pokeball_speed = -pokeball_speed
+            # Display trainer image with delay
+            if time.time() - trainer_image_timer > TRAINER_IMAGE_DELAY:
+                trainer_image_counter += 1
+                trainer_image_timer = time.time()
+                if trainer_image_counter >= len(trainer_images):
+                    trainer_image_counter = 0
+                    pokeball_drawn = True
 
-    # Aggiorna l'animazione della Poké Ball
-    pokeball_frame += 1
-    if pokeball_frame >= len(pokeball_images):
-        pokeball_frame = 0
+            # Draw trainer
+            screen.blit(trainer_images[trainer_image_counter], (100, 300))
+        # Update Poké Ball animation
+            pokeball_x += pokeball_animation_speed
+            if pokeball_x > 250:
+                pokeball_animation_speed = -pokeball_animation_speed
+            if pokeball_x < 200:
+                pokeball_animation_speed =-pokeball_animation_speed
 
-    # Verifica se la Poké Ball è stata lanciata
-    if pokeball_x > 250:
-        # Disegna il Pokémon che esce dalla Poké Ball
-        pokemon_image = pygame.transform.scale(player_pokemon_img, (50, 50))
-        screen.blit(pokemon_image, (pokeball_x, pokeball_y))
-    """
-       # Update Poké Ball animation
-    pokeball_x += pokeball_animation_speed
-    if pokeball_x > 250:
-        pokeball_animation_speed = -pokeball_animation_speed
-    if pokeball_x < 200:
-        pokeball_animation_speed = -pokeball_animation_speed
+            pokeball_frame += 1
+            if pokeball_frame >= len(pokeball_images):
+                pokeball_frame = 0
+                pokeball_drawn=True
+        # Draw Poké Ball
+            screen.blit(pokeball_images[pokeball_frame], (pokeball_x, pokeball_y))
 
-    pokeball_frame += 1
-    if pokeball_frame >= len(pokeball_images):
-        pokeball_frame = 0
+        else:
+        # Update Pokémon animation
+            pokemon_frame += 1
+            if pokemon_frame >= player_pokemon_img.get_width():
+                pokemon_frame = 0
 
-    # Update Pokémon animation
-    pokemon_frame += 1
-    if pokemon_frame >= len(player_pokemon_img):
-        pokemon_frame = 0
-
-    # Draw Poké Ball
-    screen.blit(pokeball_images[pokeball_index], (pokeball_x, pokeball_y))
+        
+        
 
     # Draw Pokémon
-    if pokeball_index == len(pokeball_images) - 1:
-        screen.blit(player_pokemon_img, player_pokemon_rect)
-        pygame.time.delay(1000)
-
-    # Update indices
-    pokeball_index += 1
-    if pokeball_index >= len(pokeball_images):
-        pokeball_index = 0
-
-    pokemon_index += 1
-    if pokemon_index >= len(player_pokemon_img):
-        pokemon_index = 0
-
-    # Suona il verso del Pokémon
-    if pokeball_index == len(pokeball_images) - 1:
-        pokemon_cry.play()
-    
-    #screen.blit(player_pokemon_img, player_pokemon_rect)
-    
-        
+            if pokeball_index == len(pokeball_images) - 1:
             
+                screen.blit(player_pokemon_img, player_pokemon_rect)
+                pokemon_drawn=True
+        # Update indices
+            pokeball_index += 1
+            if pokeball_index >= len(pokeball_images):
+                pokeball_index = 0
 
-   
+            pokemon_index += 1
+            if pokemon_index >= player_pokemon_img.get_width():
+                pokemon_index = 0
 
-    # Disegna i Pokémon
-    screen.blit(player_riq_img,player_riq_rect)
-    
-    health_bar_color_gioc = (0,255,0)
-    if (life_point/poke_life)*100< 50:
-        health_bar_color_gioc = (255, 128, 0)  # arancione
-    if (life_point/poke_life)*100< 20:
-        health_bar_color_gioc = (255,0,0)
+        # Suona il verso del Pokémon
+            if pokeball_index == len(pokeball_images) - 1:
+                pokemon_cry.play()
+    else:
+        screen.blit(player_pokemon_img, player_pokemon_rect)
+        screen.blit(player_riq_img,player_riq_rect)
+        
+        health_bar_color_gioc = (0,255,0)
+        if (life_point/poke_life)*100< 50:
+            health_bar_color_gioc = (255, 128, 0)  # arancione
+        if (life_point/poke_life)*100< 20:
+            health_bar_color_gioc = (255,0,0)
 
-    health_bar_surface_gioc = pygame.Surface((HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
-    health_bar_surface_gioc.fill(health_bar_color_gioc)
-    health_bar_surface_gioc.fill((0,0,0), (0, 0, HEALTH_BAR_WIDTH - (life_point / poke_life) * HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
-    screen.blit(health_bar_surface_gioc, (100, 257))
+        health_bar_surface_gioc = pygame.Surface((HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
+        health_bar_surface_gioc.fill(health_bar_color_gioc)
+        health_bar_surface_gioc.fill((0,0,0), (0, 0, HEALTH_BAR_WIDTH - (life_point / poke_life) * HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
+        screen.blit(health_bar_surface_gioc, (100, 257))
 
-    pygame.draw.rect(screen, (72, 139, 240), (6.5, 290, 192, 6))
-    #screen.blit(player_pokemon_img,player_pokemon_rect)
-    screen.blit(player_info_text, (50, 238))
-    screen.blit(player_life,(104,272))
-
-
-    
+        pygame.draw.rect(screen, (72, 139, 240), (6.5, 290, 192, 6))
+        #screen.blit(player_pokemon_img,player_pokemon_rect)
+        screen.blit(player_info_text, (50, 238))
+        screen.blit(player_life,(104,272))
+        
+    #alt+11 = ♂  alt+12 = ♀
+        # Disegna i pulsanti
+        if not battle_mode and not pokemon_mode and not bag_mode and not run_mode and not health_mode and not pokeballz_mode and not poke1_mode and not poke2_mode and not poke3_mode and not poke4_mode and not poke5_mode and not poke6_mode:
+            draw_button(attack_button, 'FIGHT', (104, 4, 4))
+            draw_button(bag_button, 'BAG', (128,69,10))
+            draw_button(run_button, 'RUN', (10, 69, 128))
+            draw_button(poke_button, 'POKéMON', (10,128,69))
+        elif battle_mode:
+            draw_button(move1_button, 'Move 1',(71, 82, 99))
+            draw_button(move2_button,'Move 2',(71, 82, 99))
+            draw_button(move3_button, 'Move 3',(71, 82, 99))
+            draw_button(move4_button, 'Move 4',(71, 82, 99))
+        elif pokemon_mode:
+            draw_button(pokemon1_button, 'Pokémon 1',(71, 82, 99))
+            draw_button(pokemon2_button, 'Pokémon 2',(71, 82, 99))
+            draw_button(pokemon3_button, 'Pokémon 3',(71, 82, 99))
+            draw_button(pokemon4_button, 'Pokémon 4',(71, 82, 99))
+            draw_button(pokemon5_button, 'Pokémon 5',(71, 82, 99))
+            draw_button(pokemon6_button, 'Pokémon 6',(71, 82, 99))
+        elif poke1_mode:
+            message = f"{name_gioc} è il momento di rientrare...  \n Pietro scelgote!"
+            panel = font.render(message, True, (0, 0, 0))
+            panel_rect = panel.get_rect(center = (400, 360))
+            screen.blit(panel, panel_rect)
+        elif poke2_mode:
+            message = f"{name_gioc} è il momento di rientrare... \n  Pietro scelgote!"
+            panel = font.render(message, True, (0, 0, 0))
+            panel_rect = panel.get_rect(center = (400, 360))
+            screen.blit(panel, panel_rect)
+        elif poke3_mode:
+            message = f"{name_gioc} è il momento di rientrare... \n Pietro scelgote!"
+            panel = font.render(message, True, (0, 0, 0))
+            panel_rect = panel.get_rect(center = (400, 360))
+            screen.blit(panel, panel_rect)
+        elif poke4_mode:
+            message = f"{name_gioc} è il momento di rientrare... \n Pietro scelgote!"
+            panel = font.render(message, True, (0, 0, 0))
+            panel_rect = panel.get_rect(center = (400, 360))
+            screen.blit(panel, panel_rect)
+        elif poke5_mode:
+            message = f"{name_gioc} è il momento di rientrare...  \n Pietro scelgote!"
+            panel = font.render(message, True, (0, 0, 0))
+            panel_rect = panel.get_rect(center = (400, 360))
+            screen.blit(panel, panel_rect)
+        elif poke6_mode:
+            message = f"{name_gioc} è il momento di rientrare... \n Pietro scelgote!"
+            panel = font.render(message, True, (0, 0, 0))
+            panel_rect = panel.get_rect(center = (400, 360))
+            screen.blit(panel, panel_rect)
+        elif bag_mode:
+            draw_button(health_button, 'Health',(71, 82, 99))
+            draw_button(pokeballz_button, 'Pokéball',(71, 82, 99))
+        #elif health_mode:
+            
+        #elif pokeballz_mode:
+            
+        elif run_mode:
+            message = "scampato pericolo!! ^.^ "
+            panel = font.render(message, True, (0, 0, 0))
+            panel_rect = panel.get_rect(center = (400, 360))
+            screen.blit(panel, panel_rect)
+            
+            running=False 
+            
     screen.blit(enemy_riq_img,enemy_riq_rect)
     #pygame.draw.rect(screen, (0,255,0), (607, 68, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
     screen.blit(enemy_pokemon_img,enemy_pokemon_rect)
@@ -281,73 +350,6 @@ while running:
     health_bar_surface_enemy.fill((0,0,0), (0, 0, HEALTH_BAR_WIDTH - (enemy_hp /enemy_tot_health) * HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT))
     screen.blit(health_bar_surface_enemy, (607, 68))
 
-    
-
- #alt+11 = ♂  alt+12 = ♀
-    # Disegna i pulsanti
-    if not battle_mode and not pokemon_mode and not bag_mode and not run_mode and not health_mode and not pokeballz_mode and not poke1_mode and not poke2_mode and not poke3_mode and not poke4_mode and not poke5_mode and not poke6_mode:
-        draw_button(attack_button, 'LOTTA', (104, 4, 4))
-        draw_button(bag_button, 'BORSA', (128,69,10))
-        draw_button(run_button, 'FUGA', (10, 69, 128))
-        draw_button(poke_button, 'POKéMON', (10,128,69))
-    elif battle_mode:
-        draw_button(move1_button, 'Mossa 1',(71, 82, 99))
-        draw_button(move2_button, 'Mossa 2',(71, 82, 99))
-        draw_button(move3_button, 'Mossa 3',(71, 82, 99))
-        draw_button(move4_button, 'Mossa 4',(71, 82, 99))
-    elif pokemon_mode:
-        draw_button(pokemon1_button, 'Pokémon 1',(71, 82, 99))
-        draw_button(pokemon2_button, 'Pokémon 2',(71, 82, 99))
-        draw_button(pokemon3_button, 'Pokémon 3',(71, 82, 99))
-        draw_button(pokemon4_button, 'Pokémon 4',(71, 82, 99))
-        draw_button(pokemon5_button, 'Pokémon 5',(71, 82, 99))
-        draw_button(pokemon6_button, 'Pokémon 6',(71, 82, 99))
-    elif poke1_mode:
-        message = f"{name_gioc} è il momento di rientrare...  \n Pietro scelgote!"
-        panel = font.render(message, True, (0, 0, 0))
-        panel_rect = panel.get_rect(center = (400, 360))
-        screen.blit(panel, panel_rect)
-    elif poke2_mode:
-        message = f"{name_gioc} è il momento di rientrare... \n  Pietro scelgote!"
-        panel = font.render(message, True, (0, 0, 0))
-        panel_rect = panel.get_rect(center = (400, 360))
-        screen.blit(panel, panel_rect)
-    elif poke3_mode:
-        message = f"{name_gioc} è il momento di rientrare... \n Pietro scelgote!"
-        panel = font.render(message, True, (0, 0, 0))
-        panel_rect = panel.get_rect(center = (400, 360))
-        screen.blit(panel, panel_rect)
-    elif poke4_mode:
-        message = f"{name_gioc} è il momento di rientrare... \n Pietro scelgote!"
-        panel = font.render(message, True, (0, 0, 0))
-        panel_rect = panel.get_rect(center = (400, 360))
-        screen.blit(panel, panel_rect)
-    elif poke5_mode:
-        message = f"{name_gioc} è il momento di rientrare...  \n Pietro scelgote!"
-        panel = font.render(message, True, (0, 0, 0))
-        panel_rect = panel.get_rect(center = (400, 360))
-        screen.blit(panel, panel_rect)
-    elif poke6_mode:
-        message = f"{name_gioc} è il momento di rientrare... \n Pietro scelgote!"
-        panel = font.render(message, True, (0, 0, 0))
-        panel_rect = panel.get_rect(center = (400, 360))
-        screen.blit(panel, panel_rect)
-    elif bag_mode:
-        draw_button(health_button, 'Rimedio',(71, 82, 99))
-        draw_button(pokeballz_button, 'Pokéball',(71, 82, 99))
-    #elif health_mode:
-        
-    #elif pokeballz_mode:
-        
-    elif run_mode:
-        message = "scampato pericolo!! ^.^ "
-        panel = font.render(message, True, (0, 0, 0))
-        panel_rect = panel.get_rect(center = (400, 360))
-        screen.blit(panel, panel_rect)
-        
-        running=False 
-        
-
 
     pygame.time.Clock().tick(10)
 
@@ -357,7 +359,7 @@ while running:
     #enemy_hp-=1
     if life_point <=0 or enemy_hp<=0:
         #life_point = 0
-        message = "MORTO X_X"
+        message = "DEATH X_X"
         panel = font.render(message, True, (0, 0, 0))
         
         
@@ -365,6 +367,8 @@ while running:
         screen.blit(panel, (400, 360,300,100))
         running = False
         pygame.display.update()
+    
+
 
 pygame.time.delay(1000)
 pygame.quit()
